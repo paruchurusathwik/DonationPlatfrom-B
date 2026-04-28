a3e7c94202998338103987e63c920d01438f4151
paruchurusathwik <2400031061@kluniversity.in>
2026-04-28 13:00:46 +0530

    added cors config

---
+++ src/main/java/com/donate/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.donate.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins("https://donation-platform-v2.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
